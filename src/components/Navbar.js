90a2e68adb338625d28dffa3151002aaae308c04
Rahil Shah <rahiljuly02@gmail.com>
2024-11-12 19:10:52 +0530

    redux tut code

---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+const Navbar = () => {
+    const amount = useSelector((state) => state.amount);
+    const dispatch = useDispatch();
+    return (
+        <div>
+            <div>
+                <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                    <div className="container-fluid">
+                        <a className="navbar-brand" href="/">Rahil Shah</a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li className="nav-item">
+                                    <a className="nav-link active" aria-current="page" href="/">Home</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/about">About</a>
+                                </li>
+                            </ul>
+                            <button type="button" className="btn btn-primary" disabled={true}>Your balance: {amount}</button>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
